ccc323fea20a359762a49c97964555c9f295bcf3
davidfialho <fialho.david@gmail.com>
2015-03-20 18:50:12 +0000

    as funcoes de impressao passam a imprimir num ficheiro de log

--- src/common.c
+++ src/common.c
@@ -5,44 +5,75 @@
 #include "common.h"
 
 extern int errno;
+FILE *logFile = NULL;
+
+void openLogFile() {
+	if( (logFile = fopen(".ddt.log", "a+")) == NULL) {
+		puts("ERRO: ficheiro de log não pode ser aberto");
+	}
+}
+
+void closeLogFile() {
+	if(logFile != NULL)
+		fclose(logFile);
+	logFile = NULL;
+}
 
 void putwarning(const char *format, ...) {
-
-	va_list argp;			//lista de argumentos
-	va_start(argp, format);
-	fprintf(stdout, "WARN: ");
-	vfprintf(stdout, format, argp);
-	fprintf(stdout, "\n");
-	va_end(argp);
+	openLogFile();
+	if(logFile != NULL) {
+		va_list argp;			//lista de argumentos
+		va_start(argp, format);
+		fprintf(stdout, "WARN: ");
+		vfprintf(stdout, format, argp);
+		fprintf(stdout, "\n");
+		va_end(argp);
+		closeLogFile();
+	}
 }
 
 void puterror(const char *functionName, const char *format, ...) {
-
-	va_list argp;			//lista de argumentos
-	va_start(argp, format);
-	fprintf(stderr, "ERRO: %s ", functionName);
-	vfprintf(stderr, format, argp);
-	if(errno != 0) {
-		fprintf(stderr, ": %s", strerror(errno));
+	openLogFile();
+	if(logFile != NULL) {
+		va_list argp;			//lista de argumentos
+		va_start(argp, format);
+		fprintf(stderr, "ERRO: %s ", functionName);
+		vfprintf(stderr, format, argp);
+		if(errno != 0) {
+			fprintf(stderr, ": %s", strerror(errno));
+		}
+		fprintf(stderr, "\n");
+		va_end(argp);
+		closeLogFile();
 	}
-	fprintf(stderr, "\n");
-	va_end(argp);
 }
 
 void putok(const char *format, ...) {
-	va_list argp;			//lista de argumentos
-	va_start(argp, format);
-	fprintf(stdout, "OK: ");
-	vfprintf(stdout, format, argp);
-	fprintf(stdout, "\n");
-	va_end(argp);
+	openLogFile();
+	if(logFile != NULL) {
+		va_list argp;			//lista de argumentos
+		va_start(argp, format);
+
+		fprintf(stdout, "OK: ");
+		vfprintf(stdout, format, argp);
+		fprintf(stdout, "\n");
+
+		va_end(argp);
+		closeLogFile();
+	}
 }
 
 void putdebug(const char *format, ...) {
-	va_list argp;			//lista de argumentos
-	va_start(argp, format);
-	fprintf(stdout, "DEBUG: ");
-	vfprintf(stdout, format, argp);
-	fprintf(stdout, "\n");
-	va_end(argp);
+	openLogFile();
+	if(logFile != NULL) {
+		va_list argp;			//lista de argumentos
+		va_start(argp, format);
+
+		fprintf(stdout, "DEBUG: ");
+		vfprintf(stdout, format, argp);
+		fprintf(stdout, "\n");
+
+		va_end(argp);
+		closeLogFile();
+	}
 }
